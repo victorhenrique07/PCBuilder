c53bc1b1bf2c57f2e155c16255165be0137ff51e
Victor Henrique Silva <henrictor@hotmail.com>
2023-11-09 15:53:24 -0300

    Removing DisplayExceptions.cs file and moving to DisplayTests.cs

--- app/PCBuilder.Tests/DisplayTests.cs
+++ app/PCBuilder.Tests/DisplayTests.cs
@@ -105,5 +105,31 @@ namespace PCBuilder.Tests
             // Assert
             display.Area.Should().Be(expectedArea);
         }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        public void TestIfDisplaySizeIsThrowingExceptionArgumentOutOfRangeException(decimal invalidSize)
+        {
+            Action display = () => new Display(invalidSize, new DisplayResolution("TV", 1920, 1080), PanelType.LCD, 60);
+
+            display.Should().Throw<ArgumentOutOfRangeException>();
+        }
+
+        [Fact]
+        public void TestIfResolutionIsThrowingExceptionArgumentNullException()
+        {
+            Action display = () => new Display(15m, null, PanelType.OLED, 60);
+
+            display.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void TestIfPanelTypeIsThrowingExceptionArgumentOutOfRangeException()
+        {
+            Action display = () => new Display(15m, new DisplayResolution("TV", 1920, 1080), (PanelType)999, 60);
+
+            display.Should().Throw<ArgumentOutOfRangeException>();
+        }
     }
 }
