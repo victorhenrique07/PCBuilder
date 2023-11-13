8e1f81e45de227f97170c1e0e54497c1f4578ec9
Victor Henrique Silva <henrictor@hotmail.com>
2023-11-13 12:49:37 -0300

    Adding Graphics directory, MemoryModuleCOnfiguration.cs and Range.cs

---
+++ PCBuilder/Domain/Products/Graphics/GraphicsMemory.cs
@@ -0,0 +1,52 @@
+﻿using PCBuilder.Domain.Products.MotherboardAndMemory;
+using System;
+
+namespace PCBuilder.Domain.Products.Graphics
+{
+    public class GraphicsMemory : IEquatable<GraphicsMemory>
+    {
+        public virtual MemoryChipType Type { get; protected set; }
+
+        public virtual decimal Capacity { get; protected set; }
+
+        public decimal Frequency { get; protected set; }
+
+        public virtual int BusWidth { get; protected set; }
+
+        public virtual decimal Bandwidth { get { return Frequency * BusWidth / 8m; } }
+
+        protected GraphicsMemory() { }
+
+        public GraphicsMemory(MemoryChipType type, decimal capacity, decimal frequency, int busWidth)
+        {
+            this.Type = type;
+            this.Capacity = capacity;
+            this.Frequency = frequency;
+            this.BusWidth = busWidth;
+        }
+
+        public virtual bool Equals(GraphicsMemory other)
+        {
+            if (other == null)
+                return false;
+
+            return new { this.Type, this.Capacity, this.Frequency, this.BusWidth }
+                .Equals(new { other.Type, other.Capacity, other.Frequency, other.BusWidth });
+        }
+
+        public override bool Equals(object obj)
+        {
+            return this.Equals(obj as GraphicsMemory);
+        }
+
+        public override int GetHashCode()
+        {
+            return new { this.Type, this.Capacity, this.Frequency, this.BusWidth }.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Type} {this.Capacity} GB {this.Frequency} MHz {this.BusWidth} bits";
+        }
+    }
+}
---
+++ PCBuilder/Domain/Products/Shared/Range.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace PCBuilder.Domain.Products.Shared
+{
+    public class Range<T> : IEquatable<Range<T>> where T : IComparable<T>
+    {
+        public T Begin { get; private set; }
+
+        public T End { get; private set; }
+
+        public Range(T begin, T end)
+        {
+            /* TODO: validations */
+
+            this.Begin = begin;
+            this.End = end;
+        }
+
+        public bool Contains(T value)
+        {
+            return Begin.CompareTo(value) <= 0 && End.CompareTo(value) >= 0;
+        }
+
+        public bool Equals(Range<T> other)
+        {
+            if (other == null)
+                return false;
+
+            return new { this.Begin, this.End }
+                .Equals(new { other.Begin, other.End });
+        }
+
+        public override bool Equals(object obj)
+        {
+            return this.Equals(obj as Range<T>);
+        }
+
+        public override int GetHashCode()
+        {
+            return new { this.Begin, this.End }.GetHashCode();
+        }
+    }
+}
